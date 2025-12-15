a2cbd6af936ba32edd3cf12ca1cc5e1a6b432a2e
Caleb Evans <caevans@redhat.com>
2025-12-15 13:00:09 -0700

    chore: fix version mismatch between __init__.py and pyproject.toml

--- src/cordon/__init__.py
+++ src/cordon/__init__.py
@@ -2,7 +2,7 @@ from cordon.core.config import AnalysisConfig
 from cordon.core.types import AnalysisResult, MergedBlock, ScoredWindow, TextWindow
 from cordon.pipeline import SemanticLogAnalyzer
 
-__version__ = "0.1.0"
+__version__ = "0.2.1"
 
 __all__ = [
     "SemanticLogAnalyzer",
