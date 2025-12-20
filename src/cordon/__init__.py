670b7e80233399377e36b2c5da753e54d4c2d4f6
Caleb Evans <caevans@redhat.com>
2025-12-20 10:18:05 -0700

    release: v0.3.1

--- src/cordon/__init__.py
+++ src/cordon/__init__.py
@@ -2,7 +2,7 @@ from cordon.core.config import AnalysisConfig
 from cordon.core.types import AnalysisResult, MergedBlock, ScoredWindow, TextWindow
 from cordon.pipeline import SemanticLogAnalyzer
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __all__ = [
     "SemanticLogAnalyzer",
